e8a24aef090f6ae15a37b2e07c9d0936c6567679
reuf.slamnik <reuf@maestralsolutions.com>
2014-08-25 17:24:14 +0100

    New structure

---
+++ Connected Car SDK/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name connectedCarSdkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the connectedCarSdkApp
+ */
+angular.module('connectedCarSdkApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Connected Car SDK/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name connectedCarSdkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the connectedCarSdkApp
+ */
+angular.module('connectedCarSdkApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
