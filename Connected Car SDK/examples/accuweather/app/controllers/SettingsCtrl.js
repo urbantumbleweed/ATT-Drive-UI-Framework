a678bf676767011f3f01cdac7d5e748af425c969
reuf.slamnik <reuf@maestralsolutions.com>
2014-09-04 17:59:20 +0100

    Example app - settigns page

--- Connected Car SDK/examples/accuweather/app/controllers/SettingsCtrl.js
+++ Connected Car SDK/examples/accuweather/app/controllers/SettingsCtrl.js
@@ -1,7 +1,42 @@
 ﻿var app = angular.module('app');
 
-app.controller('SettingsCtrl', ['$rootScope', '$scope',
-    function ($rootScope, $scope) {
+app.controller('SettingsCtrl', ['$rootScope', '$scope', '$modal',
+    function ($rootScope, $scope, $modal) {
+        $scope.temperature = "celsius";
+        $scope.location = true
 
-        
-    }]);
+        $scope.change = function () {
+            var modalInstance = $modal.open({
+                templateUrl: 'modalContent.html',
+                controller: ModalInstanceCtrl,
+                size: 'lg',
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+}]);
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+    //$scope.items = items;
+    //$scope.selected = {
+    //    item: $scope.items[0]
+    //};
+
+    //$scope.ok = function () {
+    //    $modalInstance.close($scope.selected.item);
+    //};
+
+    //$scope.cancel = function () {
+    //    $modalInstance.dismiss('cancel');
+    //};
+};
