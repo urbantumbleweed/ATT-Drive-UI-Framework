b08e69e9735c49b3f36351fd9ac317721ca44308
Milhad Salihi <milhadsa@maestralsolutions.com>
2014-09-04 16:26:34 +0200

    updated attDrawer directive

--- Connected Car SDK/app/scripts/controllers/drawer.js
+++ Connected Car SDK/app/scripts/controllers/drawer.js
@@ -8,15 +8,21 @@
  * Controller of the connectedCarSDK
  */
 
-angular.module('connectedCarSDK')
-    .controller('DrawerCtrl', function($rootScope, $scope) {
+angular.module('connectedCarSDK').controller('DrawerCtrl', function($rootScope, $scope) {
 
-        $rootScope.showDrawer = false;
+    $scope.title = 'Menu';
+    $scope.appLinks = [
+        { text: 'Forecast', desc: 'Watch daily forecast', href: '#/forecast' },
+        { text: 'Hourly', desc: 'Monitor hourly forecast', href: '#/hourly' },
+        { text: 'Settings', desc: 'Change app settings', href: '#/settings' },
+    ];
+    
+    $rootScope.showDrawer = false;
 
-        $scope.toggleDrawer = function() {
-            $rootScope.showDrawer = !$rootScope.showDrawer;
-        };
+    $scope.toggleDrawer = function() {
+        $rootScope.showDrawer = !$rootScope.showDrawer;
+    };
 
-    });
+});
 
 
