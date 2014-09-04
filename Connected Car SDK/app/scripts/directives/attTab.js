36bb2dd1f3d46b201218c3df04ddb24edf8cd5db
Milhad Salihi <milhadsa@maestralsolutions.com>
2014-09-04 17:52:06 +0200

    Updated sdk build and accuweather app

--- Connected Car SDK/app/scripts/directives/attTab.js
+++ Connected Car SDK/app/scripts/directives/attTab.js
@@ -1,85 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc directive
- * @name connectedCarSDK.tab.directive:attTab
- * @restrict EA
- *
- * @param {string=} heading The visible heading, or title, of the tab. Set HTML headings with {@link ui.bootstrap.tabs.directive:tabHeading tabHeading}.
- * @param {string=} select An expression to evaluate when the tab is selected.
- * @param {boolean=} active A binding, telling whether or not this tab is selected.
- * @param {boolean=} disabled A binding, telling whether or not this tab is disabled.
- *
- * @description
- * Creates a tab with a heading and content. Must be placed within a {@link ui.bootstrap.tabs.directive:tabset tabset}.
- *
- * @example
-<example module="ui.bootstrap">
-  <file name="index.html">
-    <div ng-controller="TabsDemoCtrl">
-      <button class="btn btn-small" ng-click="items[0].active = true">
-        Select item 1, using active binding
-      </button>
-      <button class="btn btn-small" ng-click="items[1].disabled = !items[1].disabled">
-        Enable/disable item 2, using disabled binding
-      </button>
-      <br />
-      <tabset>
-        <tab heading="Tab 1">First Tab</tab>
-        <tab select="alertMe()">
-          <tab-heading><i class="icon-bell"></i> Alert me!</tab-heading>
-          Second Tab, with alert callback and html heading!
-        </tab>
-        <tab ng-repeat="item in items"
-          heading="{{item.title}}"
-          disabled="item.disabled"
-          active="item.active">
-          {{item.content}}
-        </tab>
-      </tabset>
-    </div>
-  </file>
-  <file name="script.js">
-    function TabsDemoCtrl($scope) {
-      $scope.items = [
-        { title:"Dynamic Title 1", content:"Dynamic Item 0" },
-        { title:"Dynamic Title 2", content:"Dynamic Item 1", disabled: true }
-      ];
-
-      $scope.alertMe = function() {
-        setTimeout(function() {
-          alert("You've selected the alert tab!");
-        });
-      };
-    };
-  </file>
-</example>
- */
-
-/**
- * @ngdoc directive
- * @name connectedCarSDK.tab.directive:tabHeading
- * @restrict EA
- *
- * @description
- * Creates an HTML heading for a {@link connectedCarSDK.tab.directive:tab tab}. Must be placed as a child of a tab element.
- *
- * @example
-<example module="ui.bootstrap">
-  <file name="index.html">
-    <tabset>
-      <tab>
-        <tab-heading><b>HTML</b> in my titles?!</tab-heading>
-        And some content, too!
-      </tab>
-      <tab>
-        <tab-heading><i class="icon-heart"></i> Icon heading?!?</tab-heading>
-        That's right.
-      </tab>
-    </tabset>
-  </file>
-</example>
- */
 angular.module('connectedCarSDK.attTab', ['connectedCarSDK.attTabset'])
   .directive('attTab', ['$parse', function ($parse) {
       return {
@@ -176,6 +96,4 @@ angular.module('connectedCarSDK.attTab', ['connectedCarSDK.attTabset'])
           node.tagName.toLowerCase() === 'data-tab-heading'
         );
     }
-})
-
-;
+});
